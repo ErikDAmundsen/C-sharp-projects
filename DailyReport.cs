f50f01222c78b73cfed36329cbe35fe1e153afe4
erikdamundsen@gmail.com <erikdamundsen@gmail.com>
2018-12-10 15:16:20 -0800

    Create Day report

---
+++ DailyReport.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+
+
+namespace DailyReport
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine ( "The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("1.What course are you on?");
+            string course = Console.ReadLine();
+
+            Console.WriteLine("2.What page are you on?");
+            int pageNo;
+            string input = Console.ReadLine();
+            if (Int32.TryParse(input, out pageNo)) ;
+            else
+            {
+                Console.WriteLine("Please only use numerals to answer.");
+                Console.WriteLine("2.What page are you on?");
+
+                string input2 = Console.ReadLine();
+                if (Int32.TryParse(input, out pageNo)) ;
+                else Console.WriteLine("Please only use numerals to answer.");
+            }
+
+           
+
+            Console.WriteLine("3.Do you need help with anything? Please answer 'true' or 'False' ");
+            bool HelpBool = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("4.Were there any positive experiences youd like to share? Please give specifics");
+            string PosEx = Console.ReadLine();
+
+            Console.WriteLine("5.Is there any other feedback youd like to provide? Please be specific");
+            string feedback = Console.ReadLine();
+
+            Console.WriteLine("6.How many hours did you study today?");
+            int Hours = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Thank you for your answers. An instructor will respond to this shortly. Have a great day!");
+            Console.Read();
+
+
+
+        }
+    }
+}
+
---
+++ VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesAndDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // this is a simple program 
+            //Console.WriteLine("what is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is " + yourName);
+            //Console.ReadLine();
+
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("Your favorite number plus 5 is: " + total);
+            //Console.ReadLine();
+
+            bool isStudying = false;
+            byte hoursWorked = 42;
+            sbyte currentTemperature = -23;
+            char questionMark = '\u2103';
+
+            Console.WriteLine(questionMark);
+            Console.ReadLine();
+        }
+    }
+}
