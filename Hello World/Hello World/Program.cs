6eea6be1944794cf22160c8244a6c4b54b81fd45
erikdamundsen@gmail.com <erikdamundsen@gmail.com>
2018-12-10 10:01:56 -0800

    Created Acme accounting

---
+++ Hello World/Hello World/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+   class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("Welcome to Acme Accounting Systems");
+        Console.WriteLine("Remember, were accounting on you.");
+        Console.ReadLine();
+        }
+    }
+
