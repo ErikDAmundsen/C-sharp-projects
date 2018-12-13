74d87fffa0c3968618775be4acd3d724c2bd37d8
erikdamundsen@gmail.com <erikdamundsen@gmail.com>
2018-12-13 14:17:28 -0800

    re submitting

---
+++ Boolean car insurance/Boolean car insurance/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean_car_insurance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("-Please answer the following questions.-");
+            Console.WriteLine("What is your age in numbers?");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("True or False: Have you ever been convicted of a DUI?");
+            bool DUI = bool.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many speeding tickets have you had?");
+            int tickets = int.Parse(Console.ReadLine());
+
+
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
--- Math and comparison operator/Math and comparison operator/Program.cs
+++ Math and comparison operator/Math and comparison operator/Program.cs
@@ -14,16 +14,25 @@ namespace Math_and_comparison_operator
             Console.WriteLine("Anonymous Income Comparison Program");
             Console.WriteLine("--------------------------------------");
             Console.WriteLine("   ");
-            Console.WriteLine("Press Enter to begin");
 
-//Person 1
-            Console.ReadLine();
+            Console.WriteLine("What is the hourly rate of person 1");
+            decimal p1hourly = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("How many hours a week does person 1 work?");
+            decimal p1HoursWorked = decimal.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("What is the hourly rate of person 2");
+            decimal p2hourly = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("How many hours a week does person 2 work?");
+            decimal p2HoursWorked = decimal.Parse(Console.ReadLine());
+
+            //Person 1
+
             Console.WriteLine("   ");
             Console.WriteLine("Person 1");
             Console.WriteLine("---------");
-            decimal p1hourly = 15.0m;
             Console.WriteLine("Hourly rate: $" + p1hourly);
-            decimal p1HoursWorked = 46.0m;
+       
             Console.WriteLine("Hours worked per week: " + p1HoursWorked);
             System.Threading.Thread.Sleep(1200);
 
@@ -34,9 +43,9 @@ namespace Math_and_comparison_operator
             Console.WriteLine("   ");
             Console.WriteLine("Person 2");
             Console.WriteLine("---------");
-            decimal p2hourly = 75.75m;
+        
             Console.WriteLine("Hourly rate: $" + p2hourly);
-            decimal p2HoursWorked = 33.0m;
+            
             Console.WriteLine("Hours worked per week: " + p2HoursWorked);
 
 
@@ -62,14 +71,8 @@ namespace Math_and_comparison_operator
             Console.WriteLine(SalaryCompare);
             Console.WriteLine("");
 
- // Unneccesary Social political commentary
-            System.Threading.Thread.Sleep(1200);
-            Console.WriteLine("Does Person 1 make a fair and liveable wage?");
-            decimal LowIncomeThresholdSeattle = 72000.0m;
-            bool livable = (p1Salary > LowIncomeThresholdSeattle );
-
-            Console.WriteLine(SalaryCompare);
-            Console.WriteLine("");
+ 
+          
             Console.ReadLine();
 
 
