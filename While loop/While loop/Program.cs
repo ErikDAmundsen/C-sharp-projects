dd1bcea168092a6b75c79e9861a39ef1ee5f5f12
erikdamundsen@gmail.com <erikdamundsen@gmail.com>
2018-12-14 14:46:13 -0800

    mission accomplished

---
+++ While loop/While loop/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press Enter to initialize countdown!");
+            Console.ReadLine();
+            int count = 10;
+            while (count > 0)
+            {
+                Console.WriteLine(count);
+                count = count - 1;
+                System.Threading.Thread.Sleep(500);
+                if (count <= 0)
+                {
+                    Console.WriteLine("LIFT OFFFFFF!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                    System.Threading.Thread.Sleep(700);
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.Clear();
+                    System.Threading.Thread.Sleep(1000);
+
+
+
+
+                }
+            }
+            Console.WriteLine("If you forgot anything important back on earth now is the time to let us know");
+            System.Threading.Thread.Sleep(1000);
+            bool forgot = false;
+   
+
+
+        
+
+            do
+            {
+
+
+
+                Console.WriteLine("Are you sure you didn't forget anything?");
+                //forgot = bool.Parse(Console.ReadLine());
+//
+                string answer = Console.ReadLine();
+                if
+
+                    (answer.ToLower().Equals("yes"))
+                { forgot = false; }
+                else if
+
+                    (answer.ToLower().Equals("no"))
+                { forgot = true; }
+                else
+                {
+                    Console.WriteLine("Incorrect input, self destructing now.");
+                    System.Threading.Thread.Sleep(1000);
+                    Environment.Exit(-1);
+                }
+//
+            } while (forgot == false );
+
+            Console.WriteLine("OH NO, Back to earth");
+            System.Threading.Thread.Sleep(1000);
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.WriteLine("Crash Landing succesful!");
+            Console.ReadLine();
+        }
+        }
+
+        
+
+
+
+    }
+
---
+++ traffic/traffic/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace traffic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //For loop to
+
+            //            Console.WriteLine("Lets see if you went over budget this month...")
+            //;           Console.WriteLine("In dollars, How much money did you earn this month?");
+            //            Decimal earned = decimal.Parse(Console.ReadLine());
+            //            Decimal totalSpent= 0.0m;
+            //            while (earned > totalSpent)
+            //                {
+            //                Console.WriteLine("How much did you spend on rent this month?");
+            //                Console.ReadLine
+
+       
+                }
+
+            }
+            }
+
+            
+                {
+
+
+            }
+          ;
+
+
+        }
+    }
+}
