4a3de76f08da70cc71801e8c2724f7d842a143b6
erikdamundsen@gmail.com <erikdamundsen@gmail.com>
2018-12-14 09:11:55 -0800

    finish exercise

--- Boolean car insurance/Boolean car insurance/Program.cs
+++ Boolean car insurance/Boolean car insurance/Program.cs
@@ -16,12 +16,28 @@ namespace Boolean_car_insurance
             Console.WriteLine("What is your age in numbers?");
             int age = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("True or False: Have you ever been convicted of a DUI?");
+            Console.WriteLine("Is it true you have never been convicted of a DUI?");
             bool DUI = bool.Parse(Console.ReadLine());
 
             Console.WriteLine("How many speeding tickets have you had?");
             int tickets = int.Parse(Console.ReadLine());
 
+            Console.WriteLine("Please wait while we verify your information...");
+
+            System.Threading.Thread.Sleep(1200);
+
+            bool Boolage = (age > 15);
+            Console.WriteLine("You are old enough to drive: " + Boolage);
+
+            Console.WriteLine("You have never been convicted of a DUI: " + DUI);
+
+            bool tix = (tickets <= 3);
+            Console.WriteLine  (" You have 3 or fewer tickets: " + tix);
+
+
+            bool qualified = Boolage && DUI && tix;
+
+            Console.WriteLine("Are you qualified for our insurance? " + qualified);
 
             Console.ReadLine();
 
