ac8d63646febbd40f318c0cfec682e48bc6cdc43
erikdamundsen@gmail.com <erikdamundsen@gmail.com>
2019-01-14 10:37:26 -0800

    m

--- blackjack/blackjack/Card.cs
+++ blackjack/blackjack/Card.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace blackjack
 { 
-  public  class Card
+  public  struct Card
     {
 
      
---
+++ struct/struct/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @struct
+{
+    class Program
+    {
+        public struct Number
+        {
+          public decimal Amount//1. Create a struct called Number and give it the property "Amount" and have it be of data type decimal.
+            {get;set;
+                
+            } 
+           
+                }
+
+        static void Main(string[] args)
+        {
+
+            Number amo = new Number();//2. In the Main() method, create an object of data type Number and assign an amount to it.
+
+            amo.Amount = 35.35m;
+
+            Console.WriteLine("It took me " +amo.Amount +" tries to remember to put the m after the decimal"); //3. Print this amount to the console.
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+
+
+
