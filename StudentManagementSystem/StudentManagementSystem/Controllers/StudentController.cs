7fb5b83174eb300bf2b0d49989dfc623f4a259bc
erikdamundsen@gmail.com <erikdamundsen@gmail.com>
2019-02-01 13:30:44 -0800

    troubleshooting

---
+++ StudentManagementSystem/StudentManagementSystem/Controllers/StudentController.cs
@@ -0,0 +1,48 @@
+﻿using StudentManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace StudentManagementSystem.Controllers
+{
+    public class StudentController : Controller
+    {
+        private string _connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=School;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        // GET: Student
+        public ActionResult Index()
+        {
+            string queryString = "Select  * From Students";
+            List<Student> students = new List<Student>();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+                SqlDataReader = command.ExecuteReader();
+
+                while ()
+
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+                return View();
+        }
+    }
+}
