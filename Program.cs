dfcb85ef05ec191684c02f806db5a3fa65de46fa
erikdamundsen@gmail.com <erikdamundsen@gmail.com>
2019-05-10 14:08:47 -0700

    reverse function working

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+
+                Console.WriteLine("Enter a word");
+                string entered = Console.ReadLine();
+                ReverseString(entered);
+            Console.ReadLine();
+
+                void ReverseString(string str)
+                {
+
+                    char[] charArray = str.ToCharArray();
+                    for (int i = 0, j = str.Length - 1; i < j; i++, j--)
+                    {
+                        charArray[i] = str[j];
+                        charArray[j] = str[i];
+                        string reversedstring = new string(charArray);
+                        Console.WriteLine(reversedstring);
+                    }
+                }
+            }
+        }
+    }
+
