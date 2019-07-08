1205431ee8abcdcc6d27d0174965a8ff98f7107e
erikdamundsen@gmail.com <erikdamundsen@gmail.com>
2019-07-08 13:20:17 -0700

    annagram method

--- whiteboarding/whitebaord 2/whitebaord 2/Program.cs
+++ whiteboarding/whitebaord 2/whitebaord 2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -70,33 +71,67 @@ namespace whitebaord_2
             //print a times table
 
 
-         //   Console.WriteLine("Enter a whole number");
-         //   int IN = Int32.Parse(Console.ReadLine());
+            //   Console.WriteLine("Enter a whole number");
+            //   int IN = Int32.Parse(Console.ReadLine());
 
-         //   TimesT(IN);
+            //   TimesT(IN);
 
-         //   void TimesT(int INN) { 
-         //   for (int i = 1; i <= 10; i++)
-         //   {
-         //           Console.WriteLine(i * INN);
-         //   }
-         //}
+            //   void TimesT(int INN) { 
+            //   for (int i = 1; i <= 10; i++)
+            //   {
+            //           Console.WriteLine(i * INN);
+            //   }
+            //}
 
             //count the number of times a char occurs in a string
-          
-                //SortedDictionary<char, int> characterCount = new SortedDictionary<char, int>();
-                //foreach (var character in stringToCount)
-                //{ if (!characterCount.ContainsKey(character))
-                //    {
-                //        characterCount.Add(character, 1);
-                //    }
-                //else
-                //    {
-                //        characterCount[character]++;
-                //    }
 
-                //}
+            //SortedDictionary<char, int> characterCount = new SortedDictionary<char, int>();
+            //foreach (var character in stringToCount)
+            //{ if (!characterCount.ContainsKey(character))
+            //    {
+            //        characterCount.Add(character, 1);
+            //    }
+            //else
+            //    {
+            //        characterCount[character]++;
+            //    }
+
+            //}
+
+           // // are two string anagrams?
             
+           //bool areAnagram(ArrayList str1,
+           //                       ArrayList str2)
+           // {
+           //     // Get lenghts of both strings 
+           //     int n1 = str1.Count;
+           //     int n2 = str2.Count;
+
+           //     // If length of both strings is not 
+           //     // same, then they cannot be anagram 
+           //     if (n1 != n2)
+           //     {
+           //         return false;
+           //     }
+
+           //     // Sort both strings 
+           //     str1.Sort();
+           //     str2.Sort();
+
+           //     // Compare sorted strings 
+           //     for (int i = 0; i < n1; i++)
+           //     {
+           //         if (str1[i] != str2[i])
+           //         {
+           //             return false;
+           //         }
+           //     }
+
+           //     return true;
+           // }
+
+
+
             Console.ReadLine();
         }
         }
