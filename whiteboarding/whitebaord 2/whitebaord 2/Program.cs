cc6f0875f9b55d1b73491da13137055d2f4fd21d
erikdamundsen@gmail.com <erikdamundsen@gmail.com>
2019-07-19 08:37:05 -0700

    posneg method

--- whiteboarding/whitebaord 2/whitebaord 2/Program.cs
+++ whiteboarding/whitebaord 2/whitebaord 2/Program.cs
@@ -176,10 +176,15 @@ namespace whitebaord_2
             }
             string st = "A B CC D";
             Console.WriteLine(Countcharacter(st));
-               
-            
-           
 
+
+
+            Console.WriteLine("\nInput first integer:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input second integer:");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Check if one is negative and one is positive:");
+            Console.WriteLine((x < 0 && y > 0) || (x > 0 && y < 0));
 
 
             Console.ReadLine();
