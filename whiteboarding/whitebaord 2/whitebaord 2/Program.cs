df1ab4af5a81bd4cf85643daa1f8685669db4a97
erikdamundsen@gmail.com <erikdamundsen@gmail.com>
2019-08-08 09:36:31 -0700

    digit sum method

--- whiteboarding/whitebaord 2/whitebaord 2/Program.cs
+++ whiteboarding/whitebaord 2/whitebaord 2/Program.cs
@@ -243,39 +243,50 @@ namespace whitebaord_2
 
             //Console.ReadLine();
 
-            Console.WriteLine("\nSum of the first 500 prime numbers: ");
-            long sum = 0;
-            int ctr = 0;
-            int n = 2;
-            while (ctr < 500)
+            //    Console.WriteLine("\nSum of the first 500 prime numbers: ");
+            //    long sum = 0;
+            //    int ctr = 0;
+            //    int n = 2;
+            //    while (ctr < 500)
+            //    {
+            //        if (isPrime(n))
+            //        {
+            //            sum += n;
+            //            ctr++;
+            //        }
+            //        n++;
+            //    }
+
+            //    Console.WriteLine(sum.ToString());
+
+            //}
+            //public static bool isPrime(int n)
+            //{
+            //    int x = (int)Math.Floor(Math.Sqrt(n));
+
+            //    if (n == 1) return false;
+            //    if (n == 2) return true;
+
+            //    for (int i = 2; i <= x; ++i)
+            //    {
+            //        if (n % i == 0) return false;
+            //    }
+
+            //    return true;
+            //}
+
+            Console.Write("Input  a number(integer): ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            while (n != 0)
             {
-                if (isPrime(n))
-                {
-                    sum += n;
-                    ctr++;
-                }
-                n++;
+                sum += n % 10;
+                n /= 10;
             }
+            Console.WriteLine("Sum of the digits of the said integer: " + sum);
 
-            Console.WriteLine(sum.ToString());
-
-        }
-        public static bool isPrime(int n)
-        {
-            int x = (int)Math.Floor(Math.Sqrt(n));
-
-            if (n == 1) return false;
-            if (n == 2) return true;
-
-            for (int i = 2; i <= x; ++i)
-            {
-                if (n % i == 0) return false;
-            }
-
-            return true;
-        }
-
-    }
         }
     }
+}
+    
 
