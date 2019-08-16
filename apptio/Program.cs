7f77acdd1e5e88260a7e39374a70fe9a39291aa6
erikdamundsen@gmail.com <erikdamundsen@gmail.com>
2019-08-16 11:48:41 -0700

    app to track time

---
+++ apptio/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace Apptio20second
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            void beep()
+            {
+               
+                for (int i = 20; i>=0; i--)
+                {
+                    Console.WriteLine(i);
+                    Thread.Sleep(1000);
+                }
+                 Console.Beep(700,500);
+            }      
+
+            for (int i = 1; i < 46; i++)
+            {
+                Console.WriteLine("Question " + i);
+                beep();
+                Console.Clear();
+            }
+                Console.WriteLine();
+        }
+    }
+}
