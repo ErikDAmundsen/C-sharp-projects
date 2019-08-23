68ad806e7e55e5b6fb560aa2bcd0182b5ae181e8
erikdamundsen@gmail.com <erikdamundsen@gmail.com>
2019-08-23 12:56:04 -0700

    remove duplicate function

--- whiteboarding/whitebaord 2/whitebaord 2/Program.cs
+++ whiteboarding/whitebaord 2/whitebaord 2/Program.cs
@@ -275,16 +275,47 @@ namespace whitebaord_2
             //    return true;
             //}
 
-            Console.Write("Input  a number(integer): ");
-            int n = Convert.ToInt32(Console.ReadLine());
-            int sum = 0;
-            while (n != 0)
-            {
-                sum += n % 10;
-                n /= 10;
-            }
-            Console.WriteLine("Sum of the digits of the said integer: " + sum);
+            //Console.Write("Input  a number(integer): ");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //int sum = 0;
+            //while (n != 0)
+            //{
+            //    sum += n % 10;
+            //    n /= 10;
+            //}
+            //Console.WriteLine("Sum of the digits of the said integer: " + sum);
 
+            string dup(string inp)
+            {
+                string final = string.Empty;
+                foreach(Char character in inp)
+                {
+                    final = final + character;
+                    final = final + character;
+
+                }
+                return final;
+            }
+            string st = "Eeriikk";
+            Console.WriteLine(remoDup(st));
+            Console.ReadLine();
+
+            string remoDup(string inp)
+            {
+                string final = string.Empty;
+                foreach(char character in inp)
+                {
+                    if (final.Contains(character))
+                        {
+                        continue;
+                         }
+                    else
+                    {
+                        final += character;
+                    }
+                }
+                return final;
+            }
         }
     }
 }
