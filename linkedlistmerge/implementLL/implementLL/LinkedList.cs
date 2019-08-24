44ff854d560abae2693ba55660ec031eb114c980
erikdamundsen@gmail.com <erikdamundsen@gmail.com>
2019-08-24 13:12:27 -0700

    old LL

---
+++ linkedlistmerge/implementLL/implementLL/LinkedList.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace implementLL
+{
+    class LinkedList
+    {
+        //add(object)
+        //add(int position, object)
+        //remove
+        //isEmpy
+        //isFull(return false)
+        //replace(int positinol, object)
+        //getEntry
+        //contains
+        //clear
+        //getLength
+
+        private Node headNode;
+
+        public bool add(int postion, object newEntry)
+        {
+            Node tmp = headNode;
+            if (position >= 1)
+            {
+                Node newNode = new Node(newEntry);
+                if (isEmpty() || position == 1)
+                {
+                    newNode.setNextNode(tmp);
+                    headNode = newNode;
+                    return true;
+                }
+                else
+                {
+                    for (int i = 0; i < (position - 1); i++)
+                    {
+                        tmp = tmp.getNextNode();
+                    }
+
+                }
+            }
+        }
+
+        public bool add(object newEntry)
+        {
+        
+        }
+        public bool isEmpty()
+        {
+            return false;
+        }
+    }
+}
---
+++ linkedlistmerge/implementLL/implementLL/Node.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace implementLL
+{
+    class Node
+    {
+
+        private object data;
+        private Node next;
+
+
+        public Node()
+        {
+            data = null;
+            next = null;
+        }
+        public Node(object o)
+        {
+            data = o;
+            next = null;
+        }
+        public Node(object o,Node n)
+        {
+            data = o;
+            next = n;
+        }
+
+        public void setNextNode(Node n)
+        { next = n;
+        }
+        public Node getNextNode()
+        {
+            return next;
+        }
+
+        public void setData(object o)
+        {
+            data = o;
+        }
+
+        public object getData()
+        {
+            return data;
+        }
+    }
+}
